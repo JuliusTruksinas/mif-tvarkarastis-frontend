adb1c511bba8022212f7287736b94575a261fa25
Julius Trukšinas <124052041+JuliusTruksinas@users.noreply.github.com>
2024-10-28 14:52:21 +0200

    MIFT-83 axios instance setup (#7)

--- src/config/Router/Router.tsx
+++ src/config/Router/Router.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import { routes } from './routes';
-import HomePage from '../pages/public/HomePage';
-import LoginPage from '../pages/public/LoginPage';
-import RegisterPage from '../pages/public/RegisterPage';
+import HomePage from '../../pages/public/HomePage';
+import LoginPage from '../../pages/public/LoginPage';
+import RegisterPage from '../../pages/public/RegisterPage';
 
 const getRoutes = () => {
   return (
