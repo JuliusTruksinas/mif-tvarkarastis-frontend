7ca5252c9b61c64a63c0754b80afb3a2a2963055
GustavasMarcinkevicius <gustavas.marcinkevicius@gmail.com>
2024-12-28 12:18:40 +0200

    MIFT-154 Ability to choose between waze/googleMaps and all of its logic (#42)

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,4 +1,4 @@
-import moment from 'moment-timezone';
+import moment from 'moment';
 
 export const extractDate = (isoString: string) =>
   moment(isoString).format('YYYY-MM-DD');
@@ -6,4 +6,12 @@ export const extractDate = (isoString: string) =>
 export const extractTime = (isoString: string) => {
   const userTimeZone = moment.tz.guess();
   return moment.utc(isoString).tz(userTimeZone).format('HH:mm');
+};
+
+export const convertToSecondsInTimezone = (milliseconds: number): number => {
+  const now = new Date();
+  const timeZoneOffset = now.getTimezoneOffset() * 60;
+  const CorrentUnixTimeStamp = milliseconds / 1000 - timeZoneOffset;
+
+  return CorrentUnixTimeStamp;
 };
